8f0ab6b5f027e92c26742d6d3da59d13f90fca9a
Pekka <nospam@astudios.org>
2017-09-13 20:26:16 +0300

    Started on migration generation

--- zoomOut/backend/create.php
+++ zoomOut/backend/create.php
@@ -7,8 +7,11 @@ $data = json_decode(file_get_contents('php://input'), true)['data'];
 if ($data['type'] === 'model')
 {
   $object = $data['object'];
+
   $fields = $object['databaseFields'];
   $className = $object['className'];
+  $isIndex = isset($object['isIndex']);
+
   $modelFilePath = $rootPath . 'app/' . $className . '.php';
 
   if (file_exists($modelFilePath))
@@ -34,6 +37,17 @@ if ($data['type'] === 'model')
   $template = str_replace('MODELCLASSNAME', $className, $template);
   $template = str_replace('// FILLABLE', $fillableCode, $template);
   file_put_contents($modelFilePath, $template);
+
+  // Migration
+  if ( ! empty($fields))
+  {
+    $tableName = lcfirst($className);
+    $timestampString = date('Y_n_d') . '_000000';
+    $migrationFilePath = $rootPath . 'database/migrations/' . $timestampString . '_create_' . $tableName . '_table.php';
+    $template = file_get_contents(__DIR__ . '/templates/migration.php');
+    $template = str_replace('TABLENAME', $tableName, $template);
+    file_put_contents($migrationFilePath, $template);
+  }
 }
 
 die(json_encode([
